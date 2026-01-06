2a76901ab5110d1b2df61fe8b3160216d3dd69a6
itsgrimetime <990274+itsgrimetime@users.noreply.github.com>
2026-01-06 08:17:32 -0800

    vi: add matched functions from vi0801.c and vi1202.c

---
+++ src/melee/vi/vi1202.static.h
@@ -0,0 +1,14 @@
+#ifndef __GALE01_321014
+#define __GALE01_321014
+
+#include "vi1202.h" // IWYU pragma: export
+
+#include <placeholder.h>
+
+#include <baselib/jobj.h>
+
+/* 4D7040 */ extern void** un_804D7040;
+/* 4D704C */ extern HSD_JObj* un_804D704C;
+/* 4DE140 */ static f32 un_804DE140;
+
+#endif
